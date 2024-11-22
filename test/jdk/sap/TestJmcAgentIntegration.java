d211758c778bfdb8f58bc1d61856db39ae3f0498
Ralf Schmelter <ralf.schmelter@sap.com>
2024-11-22 11:44:49 +0100

    Added jtreg test which wraps the SAP jmc agent tests from the test jar.

---
+++ test/jdk/sap/TestJmcAgentIntegration.java
@@ -0,0 +1,31 @@
+/**
+ * @test
+ * @summary Runs the test for the jcm agent integration.
+ *
+ * @run main/othervm -Dcp=agent-1.0.1-SNAPSHOT-sap-tests.jar TestJmcAgentIntegration
+ */
+
+import java.lang.reflect.Method;
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class TestJmcAgentIntegration {
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("useJmcAgentOption", "true");
+        System.setProperty("traceExecs", "true");
+        File file = new File(System.getProperty("test.src") + "/agent-1.0.1-SNAPSHOT-sap-tests.jar");
+        URL url = file.toURI().toURL();
+        String classPath = System.getProperty("java.class.path", "");
+        if (classPath.length() == 0) {
+            System.setProperty("java.class.path", file.toString());
+        } else {
+            System.setProperty("java.class.path", classPath + System.getProperty("path.separator") + file.toString());
+        }
+        URLClassLoader cl = new URLClassLoader(new URL[] {url}, TestJmcAgentIntegration.class.getClassLoader());
+        Class<?> testClass = Class.forName("org.openjdk.jmc.agent.sap.test.TestRunner", true, cl);
+        Method mainMethod = testClass.getDeclaredMethod("main", String[].class);
+        mainMethod.invoke(null, new Object[] {new String[0]});
+    }
+}
